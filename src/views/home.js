5004338ba1af1b1741d2ef731da5631e075e4d75
Nikki Sollid <nikki.sollid@learnify.se>
2017-10-18 08:24:31 +0200

    - added gui for second stage

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,17 @@
 export class Home{
     constructor(){
         this.stage = 1;
+        this.selectedLanguage = null;
     }
 
     attached(){
         
+    }
+    changeLangugage(){
+        console.log(this.selectedLanguage);
+    }
+
+    next(){
+        this.stage++;
     }
 }
